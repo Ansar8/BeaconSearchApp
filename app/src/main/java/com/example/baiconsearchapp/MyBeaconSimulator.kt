2066d6d208a7400677ee2c0bb592ae44190e2dda
Ansar Kadyrov <j423252@gmail.com>
2021-02-18 10:14:02 +0300

    Add random UUID for simulated beacons.

--- app/src/main/java/com/example/baiconsearchapp/MyBeaconSimulator.kt
+++ app/src/main/java/com/example/baiconsearchapp/MyBeaconSimulator.kt
@@ -2,17 +2,32 @@ package com.example.baiconsearchapp
 
 import org.altbeacon.beacon.Beacon
 import org.altbeacon.beacon.simulator.BeaconSimulator
+import java.util.*
 
 class MyBeaconSimulator: BeaconSimulator {
 
     override fun getBeacons(): MutableList<Beacon> {
         return mutableListOf(
             Beacon.Builder()
-                .setId1("DF7E1C79-43E9-44FF-886F-1D1F7DA6997A")
+                .setId1(UUID.randomUUID().toString())
                 .setId2("1")
                 .setId3("1")
                 .setRssi(-55)
                 .setTxPower(-45)
+                .build(),
+            Beacon.Builder()
+                .setId1(UUID.randomUUID().toString())
+                .setId2("3")
+                .setId3("4")
+                .setRssi(-50)
+                .setTxPower(-59)
+                .build(),
+            Beacon.Builder()
+                .setId1(UUID.randomUUID().toString())
+                .setId2("7")
+                .setId3("8")
+                .setRssi(-55)
+                .setTxPower(-55)
                 .build()
         )
     }
